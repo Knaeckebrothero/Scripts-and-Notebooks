fc4462aef1f0d0b95bf57a0aff9689b86b551797
Ghost <OverlyGenericAddress@pm.me>
2025-04-01 14:31:24 +0200

    A way to get the client ip for a streamlit app

---
+++ streamlit_get_client_ip.py
@@ -0,0 +1,23 @@
+from streamlit import runtime
+from streamlit.runtime.scriptrunner import get_script_run_ctx
+
+
+def get_remote_ip() -> str:
+    try:
+        ctx = get_script_run_ctx()
+        if ctx is None:
+            return None
+
+        session_info = runtime.get_instance().get_client(ctx.session_id)
+        if session_info is None:
+            return None
+    except Exception as e:
+        return None
+
+    return session_info.request.remote_ip
+
+
+import streamlit as st
+
+st.title("What is my IP address?")
+st.markdown(f"The remote ip is {get_remote_ip()}")
